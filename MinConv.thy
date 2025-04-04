3c549cfeffaafc5e6e1f44b8233de474f499bf21
meesu <monu.da.vinci@gmail.com>
2025-04-05 04:37:18 +0530

    min_conv_rec proof outline added

--- MinConv.thy
+++ MinConv.thy
@@ -8,8 +8,33 @@ lemma min_conv_arg_swap:
 
 thm min_conv_base
 
+
+(*
+Suppose that X contains f(k − 1, l) + f(k,l − 1) − 1 points. Let Y be the set
+of left endpoints of (k − 1)-cups of X. If X \ Y contains f(k − 1, l) points, then it
+contains an l-cap. Otherwise, Y contains f(k,l−1) points. Suppose that Y contains
+an (l −1)-cap {(xi1 , yi1 ),(xi2 , yi2 ),... ,(xil−1 , yil−1 )}. Let {(xj1 , yj1 ),(xj2 , yj2 ),... ,
+(xjk−1 , yjk−1 )} be a (k − 1)-cup with il−1 = j1. A quick sketch then shows that
+either (xil−2 , yil−2 ) can be added to the (k − 1)-cup to create a k-cup or (xj2 , yj2 )
+can be added to the (l − 1)-cap to create an l-cap.
+*)
 lemma min_conv_rec:
-  "min_conv k l \<le> min_conv (k - 1) l + min_conv k (l - 1) - 1" sorry
+  "min_conv k l \<le> min_conv (k - 1) l + min_conv k (l - 1) - 1"
+proof-
+  obtain X where xo: "general_pos X" "card X = min_conv (k - 1) l + min_conv k (l - 1) - 1" 
+    using genpos_ex gpos_generalpos by blast
+  define Y where yo: "Y = {Min xs | xs. xs \<subseteq> X \<and> cup (k-1) (sorted_list_of_set xs)}"
+  have ysx: "Y\<subseteq>X" using xo yo sorry
+  show ?thesis
+  proof(cases "card (X-Y) \<ge> min_conv (k-1) l")
+    case True
+    then show ?thesis sorry (* show existence of l-cap in X\Y *)
+  next
+    case False
+    hence "card Y \<ge> min_conv k (l-1)" using xo(2) ysx sorry 
+    then show ?thesis sorry (* show existence of l-cap *)
+  qed
+qed
 
 thm binomial_Suc_Suc
 
